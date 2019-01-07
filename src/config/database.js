d1fcb332fba66e88a2a32fd4b71a3c3217d9793c
Carlos <carlosveizaga.jcvc@gmail.com>
2019-01-07 13:54:16 -0400

    configured database string

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 const env = require('../../env.json');
 
 //connecting to database
-const string_connection = `mongodb://${ env.host }/${ env.database_name }`;
+//const string_connection = `mongodb://${ env.host }/${ env.database_name }`;
+const string_connection = `mongodb://${ env.user_database }:${ env.password_database }@ds151354.mlab.com:51354/${ env.database_name }`;
 mongoose.connect(string_connection, {
     useCreateIndex: true,
     useNewUrlParser: true,
